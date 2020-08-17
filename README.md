5ab9bbdc0d2263e62a33d4388669eeca296bfddf
nick <nflower@asteria.com>
2020-08-17 19:42:19 +0900

    Added initial management page; TODO: Checkboxes not bound; Posts not yet sent

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ build single file exe:
 we take `$ellen_source` to be the top-level ellen directory.
 ```
 cd $ellen_source
-pyinstaller --onefile .\src\server.py --hidden-import=pkg_resources.py2_warn --add-data "./src/lib;lib" ellen
+pyinstaller --onefile .\src\server.py --hidden-import=pkg_resources.py2_warn --add-data "./src/lib;lib" -n ellen
 ```
 omitting the `--hidden-import` flag will cause errors.
 
--- src/server.py
+++ src/server.py
@@ -1,5 +1,5 @@
 import sys
-from flask import Flask, session, request
+from flask import Flask, session, request, render_template
 import json
 from lib import libellen
 from datetime import datetime, timedelta, timezone
@@ -39,6 +39,16 @@ def validate_format(obj) -> bool:
         return False
     return True
 
+def validate_management(obj) -> bool:
+    """ Given a json post to the Management endpoint, validate its format """
+    if obj is None or obj is not isinstance(obj, dict):
+        return False
+    # nf todo - validate the config format
+    d = {
+        ""
+    }
+    return True
+    
 
 @app.route('/savegorilla', methods=["POST"])
 def save_gorilla():
@@ -70,19 +80,49 @@ def save_gorilla():
         res["error"] = str(e)
         return res, 503
 
+@app.route('/healthcheck', methods=["GET"])
+def healthcheck():
+    return 'Ellen is Running'
 
-@app.route('/reload', methods=["GET"])
+
+@app.route("/", methods=["GET"])
+def mainpage():
+    return render_template("index.html", title="Management", config=libellen.CONFIG)
+
+# # # # # # # # # # # # # #
+# Management Page Routes  #
+# # # # # # # # # # # # # #
+@app.route("/manage/updateconfig", methods=["POST"])
+def post_config():
+    """ Receives new config data from the Save button """
+    j = request.json
+    res = {}
+    if not validate_management(j):
+        res["error"] = "received post data wasn't a valid Management Endpoint formatted JSON object"
+        return res, 400
+    
+    return ''
+
+@app.route("/manage/newconfig", methods=["POST"])
+def regenerate_config():
+    try:
+        os.remove("./config.ini")
+        print("Deleted existing config.ini")
+    except FileNotFoundError:
+        print("No pre-existing config.ini found. Continuing.")
+    libellen.write_default_config()
+    setup()
+    res = {"message": "Successfully regenerated new config.ini file"}
+    return res, 200
+
+@app.route('/manage/reload', methods=["GET"])
 def relod():
     """ re-reads the config.ini and reloads its settings """
     try:
         setup()
-        return {"message": "reloaded config options"}
+        return {"message": "Successfullt reloaded config options"}
     except Exception as e:
         return {"error": str(e)}, 500
-
-@app.route('/healthcheck', methods=["GET"])
-def healthcheck():
-    return 'Ellen is Running'
 
 # setup will always run, either through invocation via `Flask run` or from direct `main.
 # in case of `Flask run`, server port will be ignored and will always be `5000`. For custom port,
